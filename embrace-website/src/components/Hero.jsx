e64d2301cf002ac12daa907c8e88d61105a82725
Devasurya S <devasuryasuresh@gmail.com>
2024-08-06 21:01:40 +0530

    hero component started

--- embrace-website/src/components/Hero.jsx
+++ embrace-website/src/components/Hero.jsx
@@ -4,11 +4,13 @@ const Hero = () => {
   return (
     <section>
         <div className="container-lg">
-            <div className="row flex-lg-row-reverse">
-                <div className="col-lg-6"></div>
-                <div className="col-lg-6">
-                    <div>
-                    <h3>We Take Care of Your Brand</h3>
+            <div className="row flex-lg-row-reverse justify-content-between border">
+                <div className="col-lg-7">
+                    <div></div>
+                </div>
+                <div className="col-lg-5">
+                    <div className='py-5 my-5'>
+                    <h3 className='fw-bold display-1'>We Take Care of Your Brand</h3>
                     <p>We care about our work and we care about our clients.</p>
                     <form action="">
                         <div className='d-flex'>
