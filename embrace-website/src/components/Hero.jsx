473268dd323415ea6c4d6566da1c80b26534b3c9
Devasurya S <devasuryasuresh@gmail.com>
2024-08-08 22:05:02 +0530

    hero component finished but hero image needs more work

--- embrace-website/src/components/Hero.jsx
+++ embrace-website/src/components/Hero.jsx
@@ -4,20 +4,21 @@ const Hero = () => {
   return (
     <section>
         <div className="container-lg">
-            <div className="row flex-lg-row-reverse justify-content-between border">
-                <div className="col-lg-7">
-                    <div></div>
+            <div className="row flex-md-row-reverse justify-content-md-between">
+                <div className="col-xl-7 col-lg-6 col-md-6">
+                    <div className='heroImage'></div>
+                    {/* <img src="/public/images/hero-image.png" className='w-full heroImage' alt="" /> */}
                 </div>
-                <div className="col-lg-5">
-                    <div className='py-5 my-5'>
-                    <h3 className='fw-bold display-1'>We Take Care of Your Brand</h3>
-                    <p>We care about our work and we care about our clients.</p>
-                    <form action="">
-                        <div className='d-flex'>
-                            <input type='email' placeholder='Enter Your Email'/>
-                            <button>Lets Talk</button>
-                        </div>
-                    </form>
+                <div className="col-xl-5 col-lg-6 col-md-6">
+                    <div className='py-lg-5 my-5'>
+                        <h2 className='fw-bold font-93'>We Take Care of Your Brand</h2>
+                        <p className='fs-5'>We care about our work and we care about our clients.</p>
+                        <form action="">
+                            <div className='d-flex flex-column flex-md-row gap-2'>
+                                <input type='email' placeholder='Enter Your Email' className='form-control rounded-pill bg-light py-3'/>
+                                <button className='btn btn-primary rounded-pill widthControl py-3 px-3'>Lets Talk</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
